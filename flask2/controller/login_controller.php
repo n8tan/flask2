f164720d83ca336e419a2b7ac4d9fd8f828529eb
Keith Yung <krvy42@gmail.com>
2018-04-26 23:43:24 +0800

    4/26/2018 Commit

---
+++ flask2/controller/login_controller.php
@@ -0,0 +1,11 @@
+<?php
+
+if($_POST['inputEmail'] != NULL){
+  echo("email: " . $_POST['inputEmail'] . "<br />\n");
+  echo("inputPassword: " . $_POST['inputPassword'] . "<br />\n");
+
+}else{
+  echo "It went here.";
+}
+
+ ?>
