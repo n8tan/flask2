79c9107bd7284449c5eb6754edeb4b866ea7e2db
Keith Yung <krvy42@gmail.com>
2018-04-28 14:34:14 +0800

    Modified Index file with name field

--- flask2/index.php
+++ flask2/index.php
@@ -13,14 +13,14 @@
         <link href="css/signin.css" rel="stylesheet">
     </head>
     <body class='text-center'>
-        <form class="form-signin" method="post" action="#">
+        <form class="form-signin" method="post" action="controller/login_controller.php">
             <h1>FLASK</h1>
             </br>
             <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
             <label for="inputEmail" class="sr-only">Email address</label>
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-            <label for="inputPassword" class="sr-only">Password</label>
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                <input type="email" id="inputEmail" name="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+            <label for="inputPassword"  class="sr-only">Password</label>
+                <input type="password" id="inputPassword" name= "inputPassword" class="form-control" placeholder="Password" required>
             <button class="btn btn-lg btn-primary btn-block button" type="submit">Sign in</button>
             <a class="btn btn-lg btn-primary btn-block button" href="registerUser.php">
                 Register
